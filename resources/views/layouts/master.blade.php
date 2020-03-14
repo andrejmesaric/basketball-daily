381021287bccaac13dbbdb2cefde6b4bdc05268d
Andrej <andrej.mesaric@gmail.com>
2020-03-14 15:59:11 +0100

    Update off menu icon colors

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -77,7 +77,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           
           <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt blue"></i>
               <p>
                 Dashboard
                 <!--span class="right badge badge-danger">New</span-->
@@ -87,7 +87,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -111,7 +111,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user orange"></i>
               <p>
                 Profile
               </p>
@@ -122,7 +122,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a class="nav-link" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                               document.getElementById('logout-form').submit();">
-                <i class="nav-icon fas fa-power-off"></i>
+                <i class="nav-icon fas fa-power-off red"></i>
                 <p>
                 {{ __('Logout') }}
                 </p>
