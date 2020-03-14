0db5c4df4e5eec207c34f7f1bcade2c2ce199410
Andrej <andrej.mesaric@gmail.com>
2020-03-14 15:47:50 +0100

    Added Logout link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -86,7 +86,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Management
@@ -95,7 +95,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="fas fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
@@ -119,12 +119,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>
-                Logout
-              </p>
+            <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                <i class="nav-icon fas fa-power-off"></i>
+                <p>
+                {{ __('Logout') }}
+                </p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            
           </li>
         </ul>
       </nav>
